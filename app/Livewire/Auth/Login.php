59fcae2c7e8730fe6c8ed2f4618be473a7dbace0
Aang Wiadi <aang.wiadi@gmail.com>
2025-06-29 17:11:00 +0700

    add form login

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -1,11 +1,17 @@
 <?php
 
 namespace App\Livewire\Auth;
-
 use Livewire\Component;
+use Livewire\Attributes\Layout;
+use Illuminate\Support\Facades\Auth;
 
 class Login extends Component
 {
+    public $email = '';
+    public $password = '';
+    public $remember = false;
+
+    #[Layout('layouts.auth')]
     public function render()
     {
         return view('livewire.auth.login');
@@ -18,6 +24,11 @@ class Login extends Component
             'password' => ['required'],
         ]);
 
-        
+        if (Auth::attempt(['email' => $this->email, 'password' => $this->password], $this->remember)) {
+            session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+
+        $this->addError('email', 'The provided credentials do not match our records.');
     }
 }
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,3 +1,58 @@
-<div>
-    {{-- Success is as dangerous as failure. --}}
-</div>
+<div class="w-px-400 mx-auto mt-sm-12 mt-8">
+    <h4 class="mb-1">Welcome back! 👋</h4>
+    <p class="mb-6">Please sign-in to your account</p>
+
+    <form wire:submit="login" class="mb-6">
+      <div class="mb-6 form-control-validation">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" wire:model="email" class="form-control" id="email" name="email" placeholder="Enter your email" autofocus />
+        @error('email') <span class="text-danger">{{ $message }}</span> @enderror
+      </div>
+      <div class="form-password-toggle form-control-validation">
+        <label class="form-label" for="password">Password</label>
+        <div class="input-group input-group-merge">
+          <input type="password" wire:model="password" id="password" class="form-control" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
+          <span class="input-group-text cursor-pointer"><i class="icon-base bx bx-hide"></i></span>
+        </div>
+        @error('password') <span class="text-danger">{{ $message }}</span> @enderror
+      </div>
+      <div class="my-7 form-control-validation">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="remember-me" name="remember" />
+          <label class="form-check-label" for="remember-me">
+            Remember me
+          </label>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary d-grid w-100">Sign in</button>
+    </form>
+
+    <p class="text-center">
+      <span>New on our platform?</span>
+      {{-- <a href="{{ route('register') }}">
+        <span>Create an account</span>
+      </a> --}}
+    </p>
+
+    <div class="divider my-6">
+      <div class="divider-text">or</div>
+    </div>
+
+    <div class="d-flex justify-content-center">
+      <a href="javascript:;" class="btn btn-sm btn-icon rounded-circle btn-text-facebook me-1_5">
+        <i class="icon-base bx bxl-facebook-circle icon-20px"></i>
+      </a>
+
+      <a href="javascript:;" class="btn btn-sm btn-icon rounded-circle btn-text-twitter me-1_5">
+        <i class="icon-base bx bxl-twitter icon-20px"></i>
+      </a>
+
+      <a href="javascript:;" class="btn btn-sm btn-icon rounded-circle btn-text-github me-1_5">
+        <i class="icon-base bx bxl-github icon-20px"></i>
+      </a>
+
+      <a href="javascript:;" class="btn btn-sm btn-icon rounded-circle btn-text-google-plus">
+        <i class="icon-base bx bxl-google icon-20px"></i>
+      </a>
+    </div>
+  </div>
